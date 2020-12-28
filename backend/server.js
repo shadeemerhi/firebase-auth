55f08af457b8732fa2e595697e93a9b705d8e245
Shadee Merhi <shadmerhi@gmail.com>
2020-12-28 10:44:02 -0700

    users being received at proper endpoint'

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,54 @@
 const express = require('express');
 require("dotenv").config();
 const app = express();
+const cors = require("cors");
 const BodyParser = require('body-parser');
 const PORT = process.env.PORT || 8000;
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
+// This resets on all server saves
+const users = {};
 
 // Express configuration
 app.use(BodyParser.urlencoded({ extended: false }));
 app.use(BodyParser.json());
 app.use(express.static('public'));
 
+app.use(cors({ origin: true, credentials: true }));
+
+
 // Sample GET route
-app.get('/api/data', (req, res) => res.json({
-  message: "Seems to work!",
-}));
+app.get('/api/data', (req, res) => {
+  const idToken = req.query.token;
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then(decodedToken => {
+      console.log('user ID: ', decodedToken.uid);
+      console.log('users', users);
+      setTimeout(() => {
+        res.send(users[decodedToken.uid]);
+      }, 1200);
+    })
+    .catch(error => {
+      res.send('UNAUTHORIZED REQUEST');
+    });
+});
+
+app.post('/api/users', (req, res) => {
+  const user = req.body.user;
+  users[user.uid] = user.email;
+  console.log('users', users);
+  res.send('Thanks for the new user!');
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
